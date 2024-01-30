ba1c6061b20d3e8f7281b962a270b7b964190cfc
MeetAlgonquin <dewa0117@algonquinlive.com>
2024-01-29 21:02:23 -0500

    Step-1 Completed

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,6 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
+import * as docker from '@pulumi/docker'
+import * as containerinstance from '@pulumi/azure-native/containerinstance'
 
 
 // Import the configuration settings for the current stack.
@@ -17,10 +19,10 @@ const cpu = config.getNumber('cpu') || 1
 const memory = config.getNumber('memory') || 2
 
 // Create a resource group.
-const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`)
+const resourceGroup = new resources.ResourceGroup(${prefixName}-rg)
 
 // Create the container registry.
-const registry = new containerregistry.Registry(`cst8918a03shet0028ACR`, {
+const registry = new containerregistry.Registry(cst8918a03shet0028ACR, {
   resourceGroupName: resourceGroup.name,
   adminUserEnabled: true,
   sku: {
@@ -41,5 +43,76 @@ const registryCredentials = containerregistry
     }
   })
 
-  export const acrServer = registry.loginServer
-  export const acrUsername = registryCredentials.username
+// Define the container image for the service.
+const image = new docker.Image(${prefixName}-image, {
+    imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
+    build: {
+      context: appPath,
+      platform: 'linux/amd64'
+    },
+    registry: {
+      server: registry.loginServer,
+      username: registryCredentials.username,
+      password: registryCredentials.password
+    }
+  })
+  // Create a container group in the Azure Container App service and make it publicly accessible.
+const containerGroup = new containerinstance.ContainerGroup(
+    ${prefixName}-container-group,
+    {
+      resourceGroupName: resourceGroup.name,
+      osType: 'linux',
+      restartPolicy: 'always',
+      imageRegistryCredentials: [
+        {
+          server: registry.loginServer,
+          username: registryCredentials.username,
+          password: registryCredentials.password
+        }
+      ],
+      containers: [
+        {
+          name: imageName,
+          image: image.imageName,
+          ports: [
+            {
+              port: containerPort,
+              protocol: 'tcp'
+            }
+          ],
+          environmentVariables: [
+            {
+              name: 'PORT',
+              value: containerPort.toString()
+            },
+            {
+              name: 'WEATHER_API_KEY',
+              value: '8c862232867b0502334badbaa957fcc4'
+            }
+          ],
+          resources: {
+            requests: {
+              cpu: cpu,
+              memoryInGB: memory
+            }
+          }
+        }
+      ],
+      ipAddress: {
+        type: containerinstance.ContainerGroupIpAddressType.Public,
+        dnsNameLabel: ${imageName},
+        ports: [
+          {
+            port: publicPort,
+            protocol: 'tcp'
+          }
+        ]
+      }
+    }
+  )
+  // Export the service's IP address, hostname, and fully-qualified URL.
+export const hostname = containerGroup.ipAddress.apply(addr => addr!.fqdn!)
+export const ip = containerGroup.ipAddress.apply(addr => addr!.ip!)
+export const url = containerGroup.ipAddress.apply(
+  addr => http://${addr!.fqdn!}:${containerPort}
+)
